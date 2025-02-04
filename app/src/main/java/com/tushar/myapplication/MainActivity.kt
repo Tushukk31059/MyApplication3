04a70a24edd759b7dc9803a74d4e3e0fb93bfe08
Tushukk31059 <138555958+Tushukk31059@users.noreply.github.com>
2025-02-05 00:13:11 +0530

    view changed

--- app/src/main/java/com/tushar/myapplication/MainActivity.kt
+++ app/src/main/java/com/tushar/myapplication/MainActivity.kt
@@ -2,9 +2,12 @@ package com.tushar.myapplication
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -12,33 +15,50 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val etNumber=findViewById<TextView>(R.id.etNumber)
+        val etNumber=findViewById<EditText>(R.id.etNumber)
         val btnAdd=findViewById<Button>(R.id.btnAdd)
         val btnSubtract=findViewById<Button>(R.id.btnSubtract)
         val btnMultiply=findViewById<Button>(R.id.btnMultiply)
         val btnDivide=findViewById<Button>(R.id.btnDivide)
-        var number=etNumber.text.toString().toInt()
+
+        fun getNumber(): Int {
+            return etNumber.text.toString().toIntOrNull() ?: 0
+        }
+
         btnAdd.setOnClickListener {
-            number += 3
-            etNumber.text=number.toString()
+            val number = getNumber() + 3
+            etNumber.setText(number.toString())
+            etNumber.setSelection(etNumber.text.length)
         }
+
         btnSubtract.setOnClickListener {
-            number -= 3
-            etNumber.text=number.toString()
+            val number = getNumber() - 3
+            etNumber.setText(number.toString())
+            etNumber.setSelection(etNumber.text.length)
         }
+
         btnMultiply.setOnClickListener {
-            number*=3
-            etNumber.text=number.toString()
+            val number = getNumber() * 3
+            etNumber.setText(number.toString())
+            etNumber.setSelection(etNumber.text.length)
         }
+
         btnDivide.setOnClickListener {
-            number/=3
-            etNumber.text=number.toString()
+            val currentNumber = getNumber()
+            val number = currentNumber / 3
+            etNumber.setText(number.toString())
+            etNumber.setSelection(etNumber.text.length)
         }
+    }
+    private fun getNumber(): Int {
+        val etNumber=findViewById<EditText>(R.id.etNumber)
+        return etNumber.text.toString().toIntOrNull() ?: 0
     }
 }
